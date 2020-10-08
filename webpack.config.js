20ac7a829711d83b9d8500c028ece2f15796c094
Damian Tran <trand22@mcmaster.ca>
2020-10-07 21:45:56 -0400

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -54,57 +54,6 @@ const libConfig = {
     ]
 };
 
-const minervaConfig = {
-    entry: {
-        'minerva-cna': "./src/example/js/pages/minerva-cna.js"
-    },
-    output: {
-        filename: "minerva-cna.js",
-        path: path.resolve("C:/Users/Damian/Documents/Web/Minerva/public/app/js")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader', 'postcss-loader' ]
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [
-                    'file-loader?name=assets/images/[name].[ext]'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            "$": 'jquery',
-            'React': 'react',
-            'ReactDOM': 'react-dom',
-            'css' : 'emotion'
-        })
-    ]
-}
-
 const exampleConfig = {
     entry: {
         'home': "./src/example/js/pages/home.js",
